defcd75567557381df12c5a9326121ee6de77ada
PMOBO609 <pmobo609@gmail.com>
2026-04-15 14:48:06 +0100

    connect live backend

--- app/login/page.js
+++ app/login/page.js
@@ -8,7 +8,7 @@ export default function Login() {
 
   const handleSubmit = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://passionate-teaching-backend.onrender.com/api/auth/login',', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
